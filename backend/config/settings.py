c5fd3ac334a3013203447b2147ec5131c1833885
Nadir Manzi <nadirjmanz10@gmail.com>
2026-08-01 18:52:10 +0200

    added https on csrf trusted origin values

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -173,7 +173,7 @@ CORS_ALLOW_HEADERS = (
 )
 
 # CSRF Configuration
-CSRF_TRUSTED_ORIGINS = [FRONTEND_URL, "https://{}".format(*env.list('ALLOWED_HOSTS', default=[]))]
+CSRF_TRUSTED_ORIGINS = [FRONTEND_URL, "https://{}".format(env('ALLOWED_HOSTS', default=''))]
 
 # Auth user model
 AUTH_USER_MODEL = "users.User"
